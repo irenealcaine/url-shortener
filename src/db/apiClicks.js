91e733b974b43d9a4920d74d6733f6de4e3d5ef5
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-08-06 15:38:50 +0200

    add apiclicks nd apiurls

---
+++ src/db/apiClicks.js
@@ -0,0 +1,11 @@
+import supabase from './supabase'
+
+export async function getClicksForUrls(urlIds) {
+  const { data, error } = await supabase.from('clicks').select('*').in('url_id', urlIds)
+
+  if (error) {
+    console.log(error.message)
+    throw new Error('error')
+  }
+  return data
+}
