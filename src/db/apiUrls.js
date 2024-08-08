18687ff19e9316778800e6b342a2e8ac5bba14d6
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-08-08 15:44:00 +0200

    add create url

--- src/db/apiUrls.js
+++ src/db/apiUrls.js
@@ -27,3 +27,32 @@ export async function deleteUrl(id) {
 
   return data;
 }
+
+export async function createUrl({ title, longUrl, customUrl, user_id }, qrcode) {
+
+  const short_url = Math.random().toString(36).substring(2, 6)
+
+  const fileName = `qr-${short_url}`
+  const { error: storageError } = await supabase.storage.from('qrs').upload(fileName, qrcode)
+
+  if (storageError) throw new Error(storageError.message)
+
+  const qr = `${supabaseUrl}/storage/v1/object/public/qrs/${fileName}`
+
+
+  let { data, error } = await supabase
+    .from("urls")
+    .insert([
+      {
+        title, original_url: longUrl, custom_url: customUrl || null, user_id, short_url, qr
+      }
+    ])
+    .select()
+
+  if (error) {
+    console.error(error);
+    throw new Error("Error");
+  }
+
+  return data;
+}
